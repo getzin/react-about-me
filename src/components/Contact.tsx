ee26c1e93afd3cbf285cfc2275a12ed3f19cea2d
getzin <andreas.getzin@gmail.com>
2026-07-07 00:51:52 +0200

    Test automatic deployment

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ function Contact( { contact }: any ) {
     return (
         <div className="bg-zinc-800 hover:bg-zinc-700 rounded-2xl p-6 shadow-lg hover:shadow-xl transition duration-300">
             <h2 className="text-3xl font-bold mb-6">Contact</h2>
-            <p className="text-zinc-400 mb-6">Get in touch with me!</p>
+            <p className="text-zinc-400 mb-6">Get in touch with me! :-)</p>
 
             {contact.map((item: any) => (
                 <ContactLink
